81dcd9ff78d90810bfc7a08edd5bdeb320d12787
Rajat Shetty <rajat_shetty@live.com>
2016-02-22 15:49:38 +0530

    Checkin.....

--- gitcicd/gitcicd.Tests/Controllers/HomeControllerTest.cs
+++ gitcicd/gitcicd.Tests/Controllers/HomeControllerTest.cs
@@ -35,7 +35,7 @@ namespace gitcicd.Tests.Controllers
             ViewResult result = controller.About() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            Assert.AreEqual("Your application description .", result.ViewBag.Message);
         }
 
         [TestMethod]
